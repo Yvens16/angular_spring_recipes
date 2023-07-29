998e101f95ca6b2b167047a72dc3b8a37e60140e
Yvens16 <yvensbelaston@gmail.com>
2023-07-29 12:15:35 +0200

    init

---
+++ spring_app/src/test/java/com/yvens_belaston_recipes/spring_app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvens_belaston_recipes.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
